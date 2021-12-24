3ce35bca7dc633f8efee3af8691e512fdf7d04ee
ravi-pp <pandey.ravipp@gmail.com>
2021-12-24 16:24:11 +0530

    initial changes

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+define('quizUrl','http://localhost/gym-project/');
+define('mainUrl','https://www.gainful.com/');
+
+?>
